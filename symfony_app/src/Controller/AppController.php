dabd25bc8b0dd40849d218d6422d420e129a1455
Vladimir Morozov <freezer278@gmail.com>
2024-11-09 15:18:40 +0200

    added 1 more test case with JWT token generation and parsing

--- symfony_app/src/Controller/AppController.php
+++ symfony_app/src/Controller/AppController.php
@@ -8,6 +8,9 @@ use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 class AppController extends AbstractController
 {
     #[Route('/api/v1/hello-world', name: 'hello_world')]
@@ -24,5 +27,29 @@ class AppController extends AbstractController
         $results = $entityManager->getRepository(User::class)->findRandom30Items();
 
         return $this->json($results);
+    }
+
+    #[Route('/api/v1/jwt', name: 'jwt')]
+    public function jwt(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $key = 'some_private_jwt_key_string';
+        $userId = 321321;
+        $tokenId = 87484;
+        $currentTime = time();
+        $payload = [
+            'iss' => 'http://example.org',
+            'aud' => 'http://example.com',
+            'iat' => $currentTime,
+            'exp' => $currentTime + 3600,
+            'sub' => $userId,
+
+        ];
+        $algorithm = 'HS256';
+
+        $jwt = JWT::encode($payload, $key, $algorithm);
+
+        $decoded = (array)JWT::decode($jwt, new Key($key, $algorithm));
+
+        return $this->json($decoded);
     }
 }
